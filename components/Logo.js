5b3e0ce9d3010855f17eb32f71587f68f34f6410
Takuya Nishimoto <nishimotz@gmail.com>
2020-04-01 19:30:40 +0900

    address the Logo issues

--- components/Logo.js
+++ components/Logo.js
@@ -1,8 +1,14 @@
 function Logo(props) {
   return (
     <div id="logo">
-      <a href="http://waic.jp/">
-        <img src="http://waic.jp/cmn/img/header/logo.png" alt="ウェブアクセシビリティ基盤委員会 / WAIC: Web Accessibility Infrastructure Committee" width={334} height={77} />
+      <a href="https://waic.jp/">
+        <img
+          src="https://waic.jp/wp-content/themes/waic/images/header_logo.png"
+          srcSet="https://waic.jp/wp-content/themes/waic/images/header_logo.png 1x, https://waic.jp/wp-content/themes/waic/images/header_logo_2x.png 2x"
+          alt="ウェブアクセシビリティ基盤委員会 Web Accessibility Infrastructure Committee (WAIC)"
+          width={314} height={72}
+        >
+        </img>
       </a>
     </div>
   )
