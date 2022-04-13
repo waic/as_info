96f6ce936ff2a61e111ab97b621e6b3d89aa961b
Takuya Nishimoto <nishimotz@gmail.com>
2022-04-13 14:54:21 +0900

    20220304 in progress

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default () =>
     <li>公開日：{metadata.pub_date}</li>
     <li>更新日：{metadata.mod_date}</li>
     <li>作成者：{metadata.author}</li>
-    <li><a href=" https://waic.jp/guideline/as/#past_results">過去のアクセシビリティ サポーテッド検証結果</a></li>
+    <li><a href="https://waic.jp/guideline/as/#past_results">過去のアクセシビリティ サポーテッド検証結果</a></li>
   </ul>
   <h2>検証結果を含む達成基準</h2>
   <ul>
