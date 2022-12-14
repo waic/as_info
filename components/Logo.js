7146805bd5b3b4e58a0e24575bacd70746fc7244
Takuya Nishimoto <nishimotz@gmail.com>
2022-12-14 11:29:39 +0900

    ESLint

--- components/Logo.js
+++ components/Logo.js
@@ -1,16 +1,17 @@
+import Image from 'next/image';
+
 function Logo(props) {
   return (
     <div id="logo">
       <a href="https://waic.jp/">
-        <img
+        <Image
           src="https://waic.jp/wp-content/themes/waic/images/header_logo.png"
           srcSet="https://waic.jp/wp-content/themes/waic/images/header_logo.png 1x, https://waic.jp/wp-content/themes/waic/images/header_logo_2x.png 2x"
           alt="ウェブアクセシビリティ基盤委員会 Web Accessibility Infrastructure Committee (WAIC)"
           width={314} height={72}
-        >
-        </img>
+        />
       </a>
     </div>
   )
 }
-export default Logo
+export default Logo;
